1f3b5673abe82061dbb916db496a9140198287f8
Bernardo Rufino <bermonruf@gmail.com>
2015-11-05 18:09:51 -0200

    Bump to 0.0.2

--- pick.rb
+++ pick.rb
@@ -1,9 +1,9 @@
 class Pick < Formula
   desc "Command line picker to pipe through"
   homepage "https://github.com/bernardorufino/pick"
-  url "https://github.com/bernardorufino/pick/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "e05eceb87ea8d6045dac5bbf786d18569ff0da785db5c0177cfee12af1bcd2ee"
+  url "https://github.com/bernardorufino/pick/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "7ab8199c08ad4a49003bec3e4988457bd744480f0fe8c4419d98e1bff769aad0"
 
 
   def install
